ef90b27d8b93e0b178437e67820baa1bac5e0252
Karan K.C <60384471+kirajkc@users.noreply.github.com>
2024-03-18 11:49:28 +0900

    Update ir_read_data.py

--- ir_read_data.py
+++ ir_read_data.py
@@ -2,49 +2,46 @@
 
 import RPi.GPIO as GPIO
 import rclpy
+from rclpy.node import Node
 from std_msgs.msg import String
-import time
+
+class IrNode(Node):
+    def __init__(self):
+        super().__init__('ir_sensor_node')
+        self.ir_data_pub = self.create_publisher(String, '/ir_data_topic', 10)
+        timer_period = 0.2  # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+        
+        #declaring BCM pin 11 for left sensor
+        self.sensorLeft = 11
+        # declaring BCM pin 16 for right sensor
+        self.sensorRight = 16
+
+        # declaring the BCM mode of pins
+        GPIO.setmode(GPIO.BOARD)
+
+        # set the behavior of left sensor as input
+        GPIO.setup(self.sensorLeft, GPIO.IN)
+        # set the behavior of right sensor as input
+        GPIO.setup(self.sensorRight, GPIO.IN)
+        
+
+    def timer_callback(self):
+        # checking input on left sensor
+        left_data = "1" if GPIO.input(self.sensorLeft) else "0"
+        # checking input on right sensor
+        right_data = "1" if GPIO.input(self.sensorRight) else "0"
+        self.ir_data_pub.publish(String(data=f"({left_data}) | ({right_data})"))
 
 def main():
-    # declaring BCM pin 11 for left sensor
-    sensorLeft = 11
-    # declaring BCM pin 16 for right sensor
-    sensorRight = 16
-
-    # declaring the BCM mode of pins
-    GPIO.setmode(GPIO.BOARD)
-
-    # set the behavior of left sensor as input
-    GPIO.setup(sensorLeft, GPIO.IN)
-    # set the behavior of right sensor as input
-    GPIO.setup(sensorRight, GPIO.IN)
-
     rclpy.init()
-
-    # initialize the node
-    node = rclpy.create_node('ir_sensor_node')
-
-    # initialize the publisher
-    ir_data_pub = node.create_publisher(String, '/ir_data_topic', 10)
-
+    ir_sensor_node = IrNode()
     try:
-        while rclpy.ok():
-            # checking input on left sensor
-            left_data = "1" if GPIO.input(sensorLeft) else "0"
-            # checking input on right sensor
-            right_data = "1" if GPIO.input(sensorRight) else "0"
-            
-            # publish data in the format: (ir left data 0,1) | (ir right data 0,1)
-            ir_data_pub.publish(String(data=f"({left_data}) | ({right_data})"))
-            
-            # generate time delay of 0.2 seconds
-            time.sleep(0.2)
-
+        rclpy.spin(ir_sensor_node)
     except KeyboardInterrupt:
-        # cleanup the GPIO pins for any other program use
         GPIO.cleanup()
-        node.destroy_node()
-        rclpy.shutdown()
+        ir_sensor_node.destroy_node()
+
 
 if __name__ == '__main__':
     main()
