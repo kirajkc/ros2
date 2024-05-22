149b2f962301550f1c5f2f759d4fd12c40476c28
Karan K.C <60384471+kirajkc@users.noreply.github.com>
2024-05-22 12:11:19 +0900

    Add files via upload

--- traffic_sub.py
+++ traffic_sub.py
@@ -7,6 +7,8 @@ from rclpy.node import Node
 from std_msgs.msg import String
 import RPi.GPIO as GPIO
 import time
+from gpiozero import AngularServo
+from time import sleep
 
 class TrafficSubscriber(Node):
 
@@ -15,13 +17,16 @@ class TrafficSubscriber(Node):
         self.ir_sub = self.create_subscription(String, '/traffic_light_topic', self.traffic_callback, 10)
         self.ser = serial.Serial('/dev/ttyACM0', 9600)
         self.last_traffic_state = None
+        self.last_serial_data = None
+        self.c_r = 0
+        self.c_y = 0
+        self.c_g = 0
 
         # Setup GPIO
-        GPIO.setmode(GPIO.BCM)
-        self.servo_pin = 16  # GPIO pin 16
-        GPIO.setup(self.servo_pin, GPIO.OUT)
-        self.servo_pwm = GPIO.PWM(self.servo_pin, 50)  # 50 Hz PWM frequency
-        self.servo_pwm.start(0)
+        self.servo = AngularServo(17, min_pulse_width=0.0006, max_pulse_width=0.0023)
+
+        # Start serial reading thread
+        self.start_serial_reading_thread()
 
     def traffic_callback(self, msg: String):
         self.get_logger().info("Received traffic state: %s" % msg.data)
@@ -29,23 +34,33 @@ class TrafficSubscriber(Node):
         self.check_and_move_servo()
 
     def check_and_move_servo(self):
-        if self.last_traffic_state is not None:
-            read_serial = self.ser.readline().decode('utf-8').rstrip()
-            self.get_logger().info("Received from serial: %s" % read_serial)
-            print("Received from serial: %s" % read_serial)
-            if self.last_traffic_state == read_serial:
+        if self.last_traffic_state is not None and self.last_serial_data is not None:
+            if self.last_traffic_state == self.last_serial_data:
                 if self.last_traffic_state == 'red':
-                    self.move_servo(90)
+                    self.c_r = self.c_r + 1
+                    self.c_y = 0
+                    self.c_g = 0
+                    if self.c_r == 1:
+                        self.move_servo(90)
                 elif self.last_traffic_state == 'yellow':
-                    self.move_servo(45)
+                    self.c_y = self.c_y + 1
+                    self.c_r = 0
+                    self.c_g = 0
+                    if self.c_y == 1: 
+                        self.move_servo(45)
                 elif self.last_traffic_state == 'green':
-                    self.move_servo(0)
+                    self.c_g = self.c_g + 1
+                    self.c_r = 0
+                    self.c_y = 0
+                    if self.c_g == 1:   
+                        self.move_servo(0)
 
     def move_servo(self, angle):
-        duty_cycle = angle / 18 + 2  # Map angle to duty cycle (2 to 12)
-        self.servo_pwm.ChangeDutyCycle(duty_cycle)
-        time.sleep(1)  # Adjust this delay as needed for your servo
-        self.servo_pwm.ChangeDutyCycle(0)
+        self.get_logger().info("Servo state: %d" % angle)
+    
+        self.servo.angle = angle
+        sleep(2)
+        self.servo.detach()
 
     def start_serial_reading_thread(self):
         threading.Thread(target=self.serial_reading_thread, daemon=True).start()
@@ -54,12 +69,11 @@ class TrafficSubscriber(Node):
         while True:
             read_serial = self.ser.readline().decode('utf-8').rstrip()
             self.get_logger().info("Received from serial: %s" % read_serial)
-            print("Received from serial: %s" % read_serial)
+            self.last_serial_data = read_serial
 
 def main(args=None):
     rclpy.init(args=args)
     node = TrafficSubscriber()
-    node.start_serial_reading_thread()
     try:
         rclpy.spin(node)
     except KeyboardInterrupt:
